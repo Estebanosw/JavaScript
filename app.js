784c6e9ca772afb81d80716e25458231dc84e086
Esteban Oswaldo Prieto Riaño <oswaldo@appinit.co>
2024-09-09 07:54:39 -0500

    Creación carpeta JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log("Hola Mundo");
+
+// VARIABLES
+
+let numero1=45;
+let numero2=39;
+
+const pi=3.14156;
+
+let estado=true;
+
+let numero3 = 78;
+
+numero3 = 125;
+
+let direccion = "Calle 11 # 40-59"
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(numero3);
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+
+console.log(suma)
+
+console.log(direccion)
+
+// Concatenar valores
+console.log('El primer valor es :' + numero1);
+console.log('El segundo valor es :' + numero2);
+console.log('La suma de ' + numero1 + ' y ' + numero2 + ' es :' + suma)
+console.log('La suma de ' , numero1 , ' y ' , numero2 + ' es :' , suma)
+
+// Literal Template
+console.log(`La suma de ${numero1} y ${numero2} es: ${suma}`);
+console.log(`La resta de ${numero1} y ${numero2} es: ${resta}`);
+console.log(`La multiplicación de ${numero1} y ${numero2} es: ${multiplicacion}`);
